ab53faac5e89ce8d0b3dd785d0ebe4c0386e31d6
Julian Vargas <julianvargasalvarez@gmail.com>
2013-05-11 18:24:45 -0500

    circle moving around

---
+++ graves.js
@@ -0,0 +1,33 @@
+var canvas = document.getElementById("canvas");
+var width = parseInt(window.document.documentElement.clientWidth);
+var height = parseInt(window.document.documentElement.clientHeight);
+canvas.width = width;
+canvas.height = height;
+
+var context = canvas.getContext('2d');
+
+var x = width / 2;
+var y = height / 2;
+var radius = width * 5 / 100;
+var incrementX = 1;
+var incrementY = 1;
+
+setInterval(function() {
+  if(x + radius > width  || x - radius < 0) incrementX *= -1;
+  if(y + radius > height || y - radius < 0) incrementY *= -1;
+
+  x += incrementX;
+  y += incrementY;
+
+  context.fillStyle = '#CC2222';
+  context.fillRect(0,   0, width, height);
+
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = 'green';
+  context.fill();
+  context.lineWidth = 5;
+  context.strokeStyle = '#003300';
+  context.stroke();
+
+}, 0);
